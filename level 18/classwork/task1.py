348acf6ee497ebd35fdcadc7e12d231292b22e00
lazare928 <lz5098126@gmail.com>
2025-04-13 06:49:15 -0700

    first commit

---
+++ level 18/classwork/task1.py
@@ -0,0 +1,6 @@
+number=101
+
+# while ციკლი
+while number <= 300:
+    print(number)
+    number += 2  # ვუმატებთ 2-ს კენტი რიცხვის გამოსატანად
---
+++ level 18/homework/task3.py
@@ -0,0 +1,5 @@
+my_name = "Lazare"
+user_name = input("შეიყვანე შენი სახელი: ")
+
+if user_name.lower() == my_name.lower():
+    print("coincidence")
